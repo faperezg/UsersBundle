aac8781aa1ade44a4c929f6f458004e2e90d22db
Felipe Pérez <faperezg@gmail.com>
2016-07-03 12:28:15 -0400

    Errors corrected

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -7,8 +7,6 @@
 	class SecurityController extends BaseSecurityController {
 		protected function renderLogin (array $data) {
 			$request = $this->container->get ('request_stack')->getCurrentRequest ();
-			$session = $request->getSession ();
-			$session->set ('app.last_session_id', $session->getId ());
 			$targetPath =  $request->request->get ('_target_path', null) ? : $request->query->get ('redirect', null);
 			if ($targetPath) {
 				$data ['target_path'] = $targetPath;
