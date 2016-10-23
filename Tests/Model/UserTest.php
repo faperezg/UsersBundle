8cb017945a18e9bce0c9c5a81386dcbf8b2df0c8
Felipe A. Pérez G <faperezg@gmail.com>
2016-10-22 21:16:22 -0400

    Added validations and translations test

--- Tests/Model/UserTest.php
+++ Tests/Model/UserTest.php
@@ -2,6 +2,12 @@
 	namespace FAPerezG\UsersBundle\Tests\Model;
 
 	use FAPerezG\UsersBundle\Model\User;
+	use FAPerezG\UsersBundle\Tests\DummyUser;
+	use Symfony\Component\Translation\Loader\XliffFileLoader;
+	use Symfony\Component\Translation\Translator;
+	use Symfony\Component\Validator\Constraints\Valid;
+	use Symfony\Component\Validator\ConstraintViolation;
+	use Symfony\Component\Validator\Validation;
 
 	class UserTest extends \PHPUnit_Framework_TestCase {
 		protected function setUp () {
@@ -14,12 +20,49 @@
 			gc_collect_cycles ();
 		}
 
+		/**
+		 * @return User
+		 */
+		private function getUser () {
+			return $this->getMockForAbstractClass ('FAPerezG\UsersBundle\Model\User');
+		}
+
+		private function getEmptyDummyUser () {
+			$user = new DummyUser ();
+			$user->setEmail (null)
+				->setPlainPassword (null)
+				->setFullName (null);
+			return $user;
+		}
+
+		/**
+		 * @return \Symfony\Component\Validator\ValidatorInterface
+		 */
+		private function getValidator () {
+			return Validation::createValidatorBuilder ()
+				->addXmlMappings ([
+					__DIR__ . '/../../Resources/config/validation.xml',
+				])
+				->getValidator ();
+		}
+
+		private function getTranslators () {
+			$translators = [];
+			foreach (['en', 'es'] as $locale) {
+				$translator = new Translator ($locale);
+				$translator->addLoader ('xliff', new XliffFileLoader ());
+				$translator->addResource ('xliff', __DIR__ . "/../../Resources/translations/validators.$locale.xliff", $locale);
+				$translators [ $locale ] = $translator;
+			}
+			return $translators;
+		}
+
 		public function testEmail () {
 			$user = $this->getUser ();
 			$this->assertNull ($user->getEmail ());
 
-			$user->setEmail ('tony@mail.org');
-			$this->assertEquals ('tony@mail.org', $user->getEmail ());
+			$user->setEmail ('jack@example.org');
+			$this->assertEquals ('jack@example.org', $user->getEmail ());
 		}
 
 		public function testIsPasswordRequestNonExpired () {
@@ -63,10 +106,37 @@
 			$this->assertTrue ($user->hasRole ($newrole));
 		}
 
-		/**
-		 * @return User
-		 */
-		protected function getUser () {
-			return $this->getMockForAbstractClass ('FAPerezG\UsersBundle\Model\User');
+		public function testUserValidations () {
+			$translators = $this->getTranslators ();
+			$validator  = $this->getValidator ();
+
+			// Default test
+			$user   = $this->getEmptyDummyUser ();
+			$errors = $validator->validate ($user, new Valid (), ['FAPerezGUsers_Profile', 'FAPerezGUsers_Registration', 'Default']);
+			$this->assertEquals (3, count ($errors));
+			/** @var ConstraintViolation $error */
+			foreach ($errors as $error) {
+				$translationID = $error->getMessage ();
+				if (strpos ($translationID, 'fos_user') === 0) {
+					continue;
+				}
+				/** @var Translator $translator */
+				foreach ($translators as $locale => $translator) {
+					$translationMessage = $translator->trans ($translationID);
+					$this->assertNotEquals ($translationID, $translationMessage, "Translation ID '$translationID' not found for locale '$locale'");
+				}
+			}
+			$n = count ($errors);
+			for ($i = $n - 1; $i >= 0; $i--) {
+				if (($errors [ $i ]->getPropertyPath () == 'email') || ($errors [ $i ]->getPropertyPath () == 'plainPassword') || ($errors [ $i ]->getPropertyPath () == 'fullName')) {
+					$errors->remove ($i);
+				}
+			}
+			$this->assertEquals (0, count ($errors), 'Undetected errors found: ' . count ($errors));
+			$user->setFullName ('Jack Sparrow')
+				->setEmail ('jack@example.org')
+				->setPlainPassword ('12345678');
+			$errors = $validator->validate ($user, new Valid (), ['FAPerezGUsers_Profile', 'FAPerezGUsers_Registration', 'Default']);
+			$this->assertEquals (0, count ($errors), 'User should have no errors. Got ' . count ($errors));
 		}
 	}
